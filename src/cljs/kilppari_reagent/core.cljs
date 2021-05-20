3c7d0aae1f592cbd82925194c7ba85dab3e09a30
Joni Hiltunen <djonih@gmail.com>
2021-05-20 18:25:22 +0300

    Fix script view

--- src/cljs/kilppari_reagent/core.cljs
+++ src/cljs/kilppari_reagent/core.cljs
@@ -78,26 +78,23 @@
 
 (declare script-view)
 
-(defn script-item [instr data cur-i view-i show-active?]
-  (let [class (if (and show-active? (= cur-i view-i)) "active" "step")]
+(defn script-item [instr data active-i view-i show-active?]
+  (let [class (if (and show-active? (= active-i view-i)) "active" "step")]
     (case instr
-      :repeat [:li
+      :repeat [:li.list-group-item
                {:class class :key view-i}
                [:div
                 [:div (str ":repeat " (first data))]
-                [:div (script-view (second data) (second data) false)]]]
+                [:div (script-view (second data) nil false)]]]
 
-      [:li {:class class :key cur-i} (str instr " " (first data))])))
+      [:li.list-group-item {:class class :key view-i} (str instr " " (first data))])))
 
-(defn script-view [instructions active-index main-script?]
-  [:ul
+(defn script-view [instructions active-index show-active?]
+  [:ul.list-group
    (for [i (range (count instructions))]
      (let [[instr & data] (nth instructions i)]
-       (script-item instr data active-index i main-script?)))
-   [:li (when
-         (and main-script? (>= active-index (count instructions))
-              {:key "end" :class "active"}))
-    "end"]])
+       (script-item instr data active-index i show-active?)))
+   [:li.list-group-item {:key "end"} "end"]])
 
 (defn turtle-page []
   (fn []
